b9f9be4585bd6ca25c8cf0d4beb7f2ed01b75f2b
Tim Dams <tim.dams@artesis.be>
2013-02-19 22:37:21 +0100

    First commit

---
+++ src/sharp-arduino-serial-packet-lib/sharp-arduino-serial-packet-lib/Defines.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sharp_arduino_serial_packet_lib
+{
+    class Defines
+    {
+        public const int DEFAULT_BAUDRATE = 115200;
+
+        public const int COMMAND = 0x01;
+        public const int COMMAND_REPLY = 0x02;
+
+        public const int DATA_REQUEST = 0x11;
+        public const int DATA_BYTE = 0x12;
+        public const int DATA_INT = 0x13;
+
+        public const int DATA_ARRAY_REQUEST = 0x21;
+        public const int DATA_ARRAY = 0x22;
+
+        //Sensor types:
+
+        public const int TEMPERATURE = 0x10;
+        public const int HUMIDITY = 0x11;
+
+        public const int DISTANCE = 0x30;
+        public const int MOTORSTATUS = 0x50;
+
+        //Command IDs
+
+        public const int STOP_MOTOR_A = 0x10;
+        public const int START_MOTOR_A = 0x11;
+        public const int SET_SPEED_MOTOR_A = 0x12;
+        public const int BRAKE_MOTOR_A = 0x13;
+
+        public const int STOP_MOTOR_B = 0x15;
+        public const int START_MOTOR_B = 0x16;
+        public const int SET_SPEED_MOTOR_B = 0x17;
+        public const int BRAKE_MOTOR_B = 0x18;
+
+
+    }
+}
---
+++ src/sharp-arduino-serial-packet-lib/sharp-arduino-serial-packet-lib/Packet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sharp_arduino_serial_packet_lib
+{
+     
+    public struct Packet
+    {
+        public byte PacketType;
+        public byte NodeID;
+        public byte SensorID;
+        public byte CommandID;
+        public byte Payload;
+        public byte Parity;
+    } ;             
+}
