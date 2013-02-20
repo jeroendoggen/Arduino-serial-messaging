f083672f94693920f1b0a61298fa67b6fca9ed59
Tim Dams <tim.dams@artesis.be>
2013-02-20 18:10:39 +0100

    Removed unused directives...

--- src/sharp-arduino-serial-packet-lib/sharp-arduino-serial-packet-lib/Defines.cs
+++ src/sharp-arduino-serial-packet-lib/sharp-arduino-serial-packet-lib/Defines.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace sharp_arduino_serial_packet_lib
+﻿namespace sharp_arduino_serial_packet_lib
 {
     class Defines
     {
--- src/sharp-arduino-serial-packet-lib/sharp-arduino-serial-packet-lib/SerialPacket.cs
+++ src/sharp-arduino-serial-packet-lib/sharp-arduino-serial-packet-lib/SerialPacket.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace sharp_arduino_serial_packet_lib
 {
