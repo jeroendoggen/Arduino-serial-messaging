e590aed91db96b3624b28c7590e5983f62d8c8a0
Tim Dams <tim.dams@artesis.be>
2013-03-01 09:01:58 +0100

    Cleaned up defines

--- src/sharp-arduino-serial-packet-lib/sharp-arduino-serial-packet-lib/Defines.cs
+++ src/sharp-arduino-serial-packet-lib/sharp-arduino-serial-packet-lib/Defines.cs
@@ -4,20 +4,16 @@
     public class Defines
     {
         public const int DEFAULT_BAUDRATE = 115200;
+    }
 
-        //Sensor types:
+    //Sensor types:
+    public enum SensorTypes
+    {
 
-        public const int TEMPERATURE = 0x10;
-        public const int HUMIDITY = 0x11;
-
-        public const int DISTANCE = 0x30;
-        public const int MOTORSTATUS = 0x50;
-
-
-
-        //Command IDs
-       
-
+        TEMPERATURE = 0x10,
+        HUMIDITY = 0x11,
+        DISTANCE = 0x30,
+        MOTORSTATUS = 0x50
     }
     public enum Commands
     {
@@ -30,8 +26,8 @@
         SET_SPEED_MOTOR_B = 0x17,
         BRAKE_MOTOR_B = 0x18
     }
-    public enum PacketFields {Type, NodeID, SensorID, CommandID, Payload, Parity}
+    public enum PacketFields { Type, NodeID, SensorID, CommandID, Payload, Parity }
 
-    public enum PacketTypes { Command = 0x01, Command_Reply =0x02, Data_Request= 0x11, Data_Byte= 0x12, Data_Int=0x13, Data_Array_Request= 0x21, Data_Array= 0x22}
+    public enum PacketTypes { Command = 0x01, Command_Reply = 0x02, Data_Request = 0x11, Data_Byte = 0x12, Data_Int = 0x13, Data_Array_Request = 0x21, Data_Array = 0x22 }
 
 }
