b7cfb166db6f6db9d569d2376951aff820dc1982
Tim Dams <dams.tim@telenet.be>
2013-10-26 20:40:32 +0200

    Where'd you all go?!

---
+++ Clients/CSharpClient/sharp-arduino-serial-packet-lib/WpfTester/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using sharp_arduino_serial_packet_lib;
+
+namespace WpfTester
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            wr = new SerialReaderWriter(comport: "COM4");
+        }
+
+        private SerialReaderWriter wr;
+        private bool listening = false;
+        private void btnStartListening_Click(object sender, RoutedEventArgs e)
+        {
+            if (listening == false)
+            {
+                wr.SerialMessageReceived += wr_SerialMessageReceived;
+                wr.StartListening();
+                listening = true;
+                btnStartListening.Content = "Stop listening";
+            }
+            else
+            {
+                wr.SerialMessageReceived -= wr_SerialMessageReceived;
+                wr.StopListening();
+                listening = false;
+                btnStartListening.Content = "Start listening";
+            }
+
+        }
+
+        private delegate void UpdateUiTextDelegate(Packet recPacket);
+        void wr_SerialMessageReceived(object sender, SerialArduinoMessageEventArgs e)
+        {
+            Dispatcher.Invoke(
+                DispatcherPriority.Send,
+                new Action(
+                    () =>
+                    {
+                        lbPackets.Items.Insert(0, e.Packet);
+                    }
+                        )
+                            );
+
+        }
+
+    }
+
+    public class CommandToText : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            var incCom = (Commands)value;
+
+            return incCom.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Clients/CSharpClient/sharp-arduino-serial-packet-lib/seriallibtester/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using sharp_arduino_serial_packet_lib;
+
+namespace seriallibtester
+{
+    class Program
+    {
+        private static SerialReaderWriter r;
+        static void Main(string[] args)
+        {
+            try
+            {
+                r = new SerialReaderWriter(comport: "COM4");
+                r.SerialMessageReceived += r_SerialDataReceived;
+                r.StartListening();
+                while (true)
+                {
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void r_SerialDataReceived(object sender, SerialArduinoMessageEventArgs e)
+        {
+
+            Console.WriteLine(e.Packet.ToString());
+            Console.WriteLine("Received:{0} Corrupt: {1}",r.Statistics.ReceivedPackets, r.Statistics.CorruptPackets);
+
+        }
+    }
+}
