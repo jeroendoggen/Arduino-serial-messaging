5da693bf1111e0bd29252f7f1213ea5b58c63b80
Tim Dams <dams.tim@telenet.be>
2013-10-26 20:38:58 +0200

    Moved to correct folder...stupid stuff.

--- CSharpClient/sharp-arduino-serial-packet-lib/sharp-arduino-serial-packet-lib/Statistics.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace sharp_arduino_serial_packet_lib
-{
-    public class Statistics
-    {
-        public int ReceivedPackets { get; set; }
-        public int CorruptPackets { get; set; }
-    }
-}
