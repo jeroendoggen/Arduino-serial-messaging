2f1aef5419e05698b7296f70d67a1a80539db798
Jeroen Doggen <jeroendoggen@gmail.com>
2013-10-31 16:24:50 +0100

    added example: start motor at speed x after receiving 'start motor' message

---
+++ SerialPacket/examples/ArduinoActions/ArduinoActions.ino
@@ -0,0 +1,43 @@
+// ReceivePackets.ino - Demo application to receive data and command messages
+// Copyright 2012 Jeroen Doggen (jeroendoggen@gmail.com)
+//
+// Warning: this code is untested (not sure if it compiles...)
+
+#include "SerialPacket.h"
+#include "defines.h"
+
+SerialPacket Packet;
+
+void setup()
+{
+  Packet.begin(115200,0);                         //begin(speed,nodeID);
+}
+
+void loop()
+{
+  delay(500);
+}
+
+/*
+  SerialEvent occurs whenever a new data comes in the
+ hardware serial RX.  This routine is run between each
+time loop() runs, so using delay inside loop can delay
+response.  Multiple bytes of data may be available.
+*/
+
+void serialEvent()
+{
+  Packet.readSerialData();
+  serialActions();
+}
+
+/// Take Actions based on incoming commands:
+void serialActions(){
+  Serial.println("Starting serialActions");
+
+  if (_Packet.getCommandID() == SET_SPEED_MOTOR_A) {
+    int motorSpeed = _Packet.getPayload();
+    Serial.println("Set the motor speed to value 'motorSpeed'");
+    _Packet.sendData(COMMAND_REPLY,uint8_t(SET_SPEED_MOTOR_A));
+    }
+  }
